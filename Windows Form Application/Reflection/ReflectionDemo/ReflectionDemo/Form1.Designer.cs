16adb41e8a7d6a550479927cc78bff74c3e49b61
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-11-24 17:20:29 +0600

    WFA

---
+++ Windows Form Application/Reflection/ReflectionDemo/ReflectionDemo/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ReflectionDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTypeName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbMethod = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbPropertices = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.liConstructors = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbEvent = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Type Name : ";
+            // 
+            // txtTypeName
+            // 
+            this.txtTypeName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTypeName.Location = new System.Drawing.Point(130, 18);
+            this.txtTypeName.Name = "txtTypeName";
+            this.txtTypeName.Size = new System.Drawing.Size(452, 22);
+            this.txtTypeName.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(624, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get Type Information";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Methods";
+            // 
+            // lbMethod
+            // 
+            this.lbMethod.FormattingEnabled = true;
+            this.lbMethod.Location = new System.Drawing.Point(34, 93);
+            this.lbMethod.Name = "lbMethod";
+            this.lbMethod.Size = new System.Drawing.Size(245, 433);
+            this.lbMethod.TabIndex = 3;
+            this.lbMethod.SelectedIndexChanged += new System.EventHandler(this.lbMethod_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(294, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Propertices";
+            // 
+            // lbPropertices
+            // 
+            this.lbPropertices.FormattingEnabled = true;
+            this.lbPropertices.Location = new System.Drawing.Point(297, 93);
+            this.lbPropertices.Name = "lbPropertices";
+            this.lbPropertices.Size = new System.Drawing.Size(224, 433);
+            this.lbPropertices.TabIndex = 3;
+            this.lbPropertices.SelectedIndexChanged += new System.EventHandler(this.lbPropertices_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(540, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Constructors";
+            // 
+            // liConstructors
+            // 
+            this.liConstructors.FormattingEnabled = true;
+            this.liConstructors.Location = new System.Drawing.Point(543, 93);
+            this.liConstructors.Name = "liConstructors";
+            this.liConstructors.Size = new System.Drawing.Size(226, 433);
+            this.liConstructors.TabIndex = 3;
+            this.liConstructors.SelectedIndexChanged += new System.EventHandler(this.liConstructors_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(786, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Events";
+            // 
+            // lbEvent
+            // 
+            this.lbEvent.FormattingEnabled = true;
+            this.lbEvent.Location = new System.Drawing.Point(789, 93);
+            this.lbEvent.Name = "lbEvent";
+            this.lbEvent.Size = new System.Drawing.Size(226, 433);
+            this.lbEvent.TabIndex = 3;
+            this.lbEvent.SelectedIndexChanged += new System.EventHandler(this.liConstructors_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 556);
+            this.Controls.Add(this.lbEvent);
+            this.Controls.Add(this.liConstructors);
+            this.Controls.Add(this.lbPropertices);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbMethod);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTypeName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reflection Demo";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTypeName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbMethod;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbPropertices;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox liConstructors;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox lbEvent;
+    }
+}
+
---
+++ Windows Form Application/Reflection/ReflectionDemo/ReflectionDemo/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReflectionDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbMethod_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbPropertices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void liConstructors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userType = txtTypeName.Text;
+            Type T= Type.GetType(userType);
+
+            lbPropertices.Items.Clear();
+            lbMethod.Items.Clear();
+            liConstructors.Items.Clear();
+            lbEvent.Items.Clear();
+
+            MethodInfo[] methods = T.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                lbMethod.Items.Add(method.MemberType + " - " + method.Name);
+            }
+
+            PropertyInfo[] propertys = T.GetProperties();
+            foreach (PropertyInfo property in propertys)
+            {
+                lbPropertices.Items.Add(property.PropertyType.Name + " - " + property.Name);
+            }
+
+            ConstructorInfo[] constructors = T.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                liConstructors.Items.Add(constructor.ToString());
+                
+            }
+           
+            EventInfo[] eventInfos = T.GetEvents();
+            foreach (EventInfo eventInfo in eventInfos)
+            {
+                lbEvent.Items.Add(eventInfo.Name+" "+eventInfo.MemberType);
+            }
+
+
+        }
+    }
+}
