ca21caeb90a95b39d78e29879b2875b9679889f2
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-11-26 20:23:37 +0600

    C#

---
+++ Console Application/Practice/QueueExample/QueueExample/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q_ueueExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer()
+            {
+                Id = 101,
+                Name = "Mark",
+                Salary = 4000
+            };
+            Customer c2 = new Customer()
+            {
+                Id = 102,
+                Name = "Marry",
+                Salary = 5000
+            };
+            Customer c3 = new Customer()
+            {
+                Id = 103,
+                Name = "Dom",
+                Salary = 6000
+            };
+            Customer c4 = new Customer()
+            {
+                Id = 104,
+                Name = "Piter",
+                Salary = 7000
+            };
+            Customer c5 = new Customer()
+            {
+                Id = 105,
+                Name = "John",
+                Salary = 8000
+            };
+
+
+            Queue<Customer> queueLiCustomers= new Queue<Customer>();
+            queueLiCustomers.Enqueue(c1);
+            queueLiCustomers.Enqueue(c2);
+            queueLiCustomers.Enqueue(c3);
+            queueLiCustomers.Enqueue(c4);
+            queueLiCustomers.Enqueue(c5);
+
+            //Peek Example
+            Customer c = queueLiCustomers.Dequeue();
+
+            Console.WriteLine(c.Id + " - " + c.Name);
+            Console.WriteLine(queueLiCustomers.Count);
+
+
+
+            //Dequeue Example..........
+
+            //Customer c = queueLiCustomers.Dequeue();
+
+            //Console.WriteLine(c.Id+" - "+c.Name);
+            //Console.WriteLine(queueLiCustomers.Count);
+            //Console.WriteLine();
+            //Customer cc = queueLiCustomers.Dequeue();
+
+            //Console.WriteLine(cc.Id + " - " + cc.Name);
+            //Console.WriteLine(queueLiCustomers.Count);
+            //Console.WriteLine();
+            //Customer ccc = queueLiCustomers.Dequeue();
+
+            //Console.WriteLine(ccc.Id + " - " + ccc.Name);
+            //Console.WriteLine(queueLiCustomers.Count);
+            //Console.WriteLine();
+            //Customer cccc = queueLiCustomers.Dequeue();
+
+            //Console.WriteLine(cccc.Id + " - " + cccc.Name);
+            //Console.WriteLine(queueLiCustomers.Count);
+            //Console.WriteLine();
+            //Customer ccccc = queueLiCustomers.Dequeue();
+
+            //Console.WriteLine(cccc.Id + " - " + cccc.Name);
+            //Console.WriteLine(queueLiCustomers.Count);
+
+
+
+            //foreach (Customer customer in queueLiCustomers)
+            //{
+            //    Console.WriteLine(customer.Id+" - "+customer.Name);
+            //    Console.WriteLine(queueLiCustomers.Count);
+            //}
+
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+    }
+}
---
+++ Console Application/Practice/StackExample/StackExample/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer()
+            {
+                Id = 101,
+                Name = "Mark",
+                Salary = 4000
+            };
+            Customer c2 = new Customer()
+            {
+                Id = 102,
+                Name = "Marry",
+                Salary = 5000
+            };
+            Customer c3 = new Customer()
+            {
+                Id = 103,
+                Name = "Dom",
+                Salary = 6000
+            };
+            Customer c4 = new Customer()
+            {
+                Id = 104,
+                Name = "Piter",
+                Salary = 7000
+            };
+            Customer c5 = new Customer()
+            {
+                Id = 105,
+                Name = "John",
+                Salary = 8000
+            };
+
+            Stack<Customer> stackList=new Stack<Customer>();
+            stackList.Push(c1);
+            stackList.Push(c2);
+            stackList.Push(c3);
+            stackList.Push(c4);
+            stackList.Push(c5);
+
+            Customer cc1 = stackList.Peek();
+            Console.WriteLine(cc1.Id + " - " + cc1.Name);
+            Console.WriteLine(stackList.Count);
+
+            //Customer cc1 = stackList.Pop();
+            //Console.WriteLine(cc1.Id+" - "+cc1.Name);
+            //Console.WriteLine(stackList.Count);
+
+            //Customer cc2 = stackList.Pop();
+            //Console.WriteLine(cc2.Id + " - " + cc2.Name);
+            //Console.WriteLine(stackList.Count);
+
+            //Customer cc3 = stackList.Pop();
+            //Console.WriteLine(cc3.Id + " - " + cc3.Name);
+            //Console.WriteLine(stackList.Count);
+
+            //Customer cc4 = stackList.Pop();
+            //Console.WriteLine(cc4.Id + " - " + cc4.Name);
+            //Console.WriteLine(stackList.Count);
+
+            //Customer cc5 = stackList.Pop();
+            //Console.WriteLine(cc5.Id + " - " + cc5.Name);
+            //Console.WriteLine(stackList.Count);
+
+
+            //foreach (Customer customer in stackList)
+            //{
+            //    Console.WriteLine(customer.Id+" - "+customer.Name);
+            //    Console.WriteLine(stackList.Count);
+            //}
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+    }
+}
