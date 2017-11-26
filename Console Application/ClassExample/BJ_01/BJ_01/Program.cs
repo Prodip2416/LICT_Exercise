d456310cfcacc49f809ef3ded6259582a3dbb23d
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-11-26 20:48:32 +0600

    C#

---
+++ Console Application/ClassExample/BJ_01/BJ_01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BJ_01
+{
+    class Program
+    {
+      static System.Windows.Forms.TextBox txt = new TextBox();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is khan");
+            Console.WriteLine("I am a cultural terrorist.");
+
+            System.Windows.Forms.Form frm= new System.Windows.Forms.Form();
+
+            frm.Size= new Size(400,500);
+            frm.Text = "My Sweet Software";
+            
+            //System.Windows.Forms.TextBox txt= new TextBox();
+            txt.Location = new Point(20, 20);
+            frm.Controls.Add(txt);
+
+            System.Windows.Forms.Button btn = new Button();
+            btn.Location = new Point(20, 50);
+            btn.Text = "&Click me";
+            btn.Click += Btn_Click;
+            frm.Controls.Add(btn);
+
+            frm.ShowDialog();
+
+            Console.ReadKey();
+        }
+
+        private static void Btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(txt.Text);
+        }
+    }
+}
