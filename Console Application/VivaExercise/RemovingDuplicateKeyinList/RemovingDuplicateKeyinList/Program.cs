4f97dcf9f3e345e7adc73ea7303a755e7a9e7076
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-02-18 16:48:21 +0600

    C#

---
+++ Console Application/VivaExercise/RemovingDuplicateKeyinList/RemovingDuplicateKeyinList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemovingDuplicateKeyinList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            List<int> items=new List<int>();
+
+            string[] tokens = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                int newItem = Convert.ToInt32(tokens[i]);
+                items.Add(newItem);
+            }
+
+            List<int> Unique = items.Distinct().ToList();
+
+            foreach (int i in Unique)
+            {
+                Console.Write(i+" ");
+            }
+         
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Console Application/VivaExercise/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Like";
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            Console.WriteLine(charArray);
+            Console.ReadLine();
+        }
+    }
+}
